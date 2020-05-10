0a0d4cb769c6d2a3a15e370c90ec5a9ef4f0acc5
Hyeong Joon Suh <hjsuh18@gmail.com>
2020-05-10 18:16:44 +0900

    index.js change for new objects

--- src/components/objects/index.js
+++ src/components/objects/index.js
@@ -2,3 +2,7 @@ export * from './Terrain';
 export * from './Airplane';
 export * from './Fuel';
 export * from './Sky';
+export * from './Burger';
+export * from './Donut';
+export * from './Water';
+
